7f232960d629761f41620477d1cb27646e0e2a3b
drettnew <drettnew@gmail.com>
2016-04-07 13:33:39 +0200

    Added more shit

--- ProjectSDL2.1/GLPlayer.cpp
+++ ProjectSDL2.1/GLPlayer.cpp
@@ -10,3 +10,17 @@ GLPlayer::GLPlayer()
 GLPlayer::~GLPlayer()
 {
 }
+
+void GLPlayer::tempDraw()
+{
+}
+
+void GLPlayer::Update(PlayerState state, glm::vec3 movementVec)
+{
+
+}
+
+GLCamera GLPlayer::GetCamera()
+{
+	return this->m_camera;
+}
--- ProjectSDL2.1/GLPlayer.h
+++ ProjectSDL2.1/GLPlayer.h
@@ -1,8 +1,23 @@
 #pragma once
+#include "GLUtil.h"
+#include "GLCamera.h"
 class GLPlayer
 {
 public:
+	enum PlayerState
+	{
+		CAMERA_MOVE,
+		PLAYER_MOVE,
+		NUM_STATES
+	};
+
 	GLPlayer();
 	~GLPlayer();
+	void tempDraw();
+	void Update(PlayerState state, glm::vec3 movementVec);
+	GLCamera GetCamera();
+private:
+	glm::vec3 m_velocity;
+	GLCamera m_camera;
 };
 
