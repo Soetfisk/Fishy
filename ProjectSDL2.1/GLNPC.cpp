93b262d5fb70653545db3c391af73c963e57aa6b
Raxo94 <Oskar_swing@hotmail.com>
2016-04-14 17:30:04 +0200

    Added RNG Namespace

--- ProjectSDL2.1/GLNPC.cpp
+++ ProjectSDL2.1/GLNPC.cpp
@@ -1,5 +1,14 @@
 #include "GLNPC.h"
 
+
+GLNPC::GLNPC()
+{
+	RNG::range(-1.0f, 1.0f);
+	
+	this->transform->SetPos(glm::vec3(RNG::range(-20,20), RNG::range(-4, 4), RNG::range(-20, 20)));
+	
+}
+
 void GLNPC::update()
 {
 	;//Pass
@@ -9,3 +18,5 @@ void GLNPC::NPCDraw(GLShader & shader)
 {
 	this->Draw(shader);
 }
+
+
--- ProjectSDL2.1/GLNPC.h
+++ ProjectSDL2.1/GLNPC.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "GLModel.h" 
+#include "RNG.h"
 
 class GLNPC : public GLModel
 {
-	//1First i am going to make an npc character.
-	//2 Then i am going to give the npc different functions and states
+	//1 First i am going to make an npc character. v/DONE
+	//2 Change the position of the NPC
+	//3 Make a move around functions 
+	//4 Then i am going to give the npc different functions and states
 private:
 	enum NpcStates
 	{
@@ -18,6 +21,7 @@ private:
 	//void NpcDraw();
 
 public:	
+	GLNPC();
 	void update();
 	void NPCDraw(GLShader& shader);
 	
