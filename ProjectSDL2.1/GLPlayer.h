d6a5f7488a521f39bce2711e4f69a05c3c7a1fc2
Peao14 <peao14@student.bth.se>
2016-04-26 10:18:23 +0200

    Not done yet

--- ProjectSDL2.1/GLPlayer.h
+++ ProjectSDL2.1/GLPlayer.h
@@ -48,7 +48,13 @@ public:
 private:
 	const int DEADZONE = 8000;
 	float MOVEMENT_FRICTION = 2.0f;
-	const int MAX_SPEED = 10;
+	const int MAX_SPEED = 10;	
+	const float MIN_SPEED = 0.8f;
+	const int MAX_DASHSPEED = 20;
+	const float DASH_DURATION = 0.5f;
+	const float DASH_COOLDOWN = 2.0f;
+	const float DASH_SCALE = 0.2f;
+	const float MOVE_CAM_DISTANCE = 5.0f;
 	const float MAX_ANGLE = 75;
 	const int MAX_INPUT = glm::pow(2, 15);
 
